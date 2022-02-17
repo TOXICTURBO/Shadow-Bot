e7cafd1a116d64e756d05fe0a7dbe9665a53ec6b
TOXICTURBO <97358745+TOXICTURBO@users.noreply.github.com>
2022-02-17 16:46:38 +0530

    Update spark.js

--- spark.js
+++ spark.js
@@ -14,9 +14,8 @@ module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
  BUTTON: process.env.BUTTON_TEXT === undefined ? false : process.env.BUTTON_TEXT,
  WEBPREVIEW: process.env.FAKE_PREVIEW === undefined ? false : process.env.FAKE_PREVIEW,
- PHONE: process.env.NUMBER === undefined ? '919526808481' : process.env.NUMBER,   
+ PHONE: process.env.NUMBER === undefined ? '916380260672' : process.env.NUMBER,   
  OA_NAME: process.env.DEPLOYER === undefined ? 'undefined' : process.env.DEPLOYER,    
- THUMBNAIL: process.env.THUMBNAIL_URL === undefined ? 'https://i.imgur.com/xsKCqwZ.jpeg' : process.env.THUMBNAIL_URL,
- VERIFY: process.env.VERIFICATION_CAPTION === undefined ? '[Shadow Bot]\nCoded by @SPARK-SHADOW' : process.env.VERIFICATION_CAPTION,
- SHADOWPW: process.env._SHADOW_BOT_ACSESS_PASSWORD === undefined ? false : process.env._SHADOW_BOT_ACSESS_PASSWORD,
+ THUMBNAIL: process.env.THUMBNAIL_URL === undefined ? 'https://i.imgur.com/NLanTBv.jpeg' : process.env.THUMBNAIL_URL,
+ VERIFY: process.env.VERIFICATION_CAPTION === undefined ? '[Toxic-Shadow Bot]\nCoded by @Turbo & Jifi' : process.env.VERIFICATION_CAPTION,
 };
